4958d6f7f92499f18af36c188f5a9d6a62087229
Phil <fizz.uet@gmail.com>
2026-03-30 13:37:42 +0700

    fix: update scaling and reduce the cpu limits

--- gcloud/cloud_run.tf
+++ gcloud/cloud_run.tf
@@ -1,25 +1,71 @@
-resource "google_cloud_run_service" "test" {
+# run this as smallest size
+# allow all traffics
+# max = 1 instance
+# resource "google_cloud_run_v2_service" "test" {
+#   name     = "cloudrun-test"
+#   location = "asia-southeast1"
+
+#   template {
+#     spec {
+#       containers {
+#         image = "us-docker.pkg.dev/cloudrun/container/hello"
+
+#         resources {
+#           limits = {
+#             cpu    = "1000m"
+#             memory = "128Mi"
+#           }
+#         }
+#       }
+#     }
+
+#     metadata {
+#       annotations = {
+#         "autoscaling.knative.dev/maxScale"  = "1"
+#         "run.googleapis.com/cpu-throttling" = "true"
+#       }
+#     }
+#   }
+
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+# }
+
+
+resource "google_cloud_run_v2_service" "test" {
   name     = "cloudrun-test"
   location = "asia-southeast1"
 
   template {
-    spec {
-      containers {
-        image = "us-docker.pkg.dev/cloudrun/container/hello"
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 1
+    }
+
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+      resources {
+        limits = {
+          cpu    = "1000m"
+          memory = "128Mi"
+        }
+
+        cpu_idle = true
       }
     }
   }
 
   traffic {
-    percent         = 100
-    latest_revision = true
+    percent = 100
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
   }
 }
 
-
 resource "google_cloud_run_service_iam_member" "public" {
-  service  = google_cloud_run_service.test.name
-  location = google_cloud_run_service.test.location
+  service  = google_cloud_run_v2_service.test.name
+  location = google_cloud_run_v2_service.test.location
   role     = "roles/run.invoker"
   member   = "allUsers"
 }
